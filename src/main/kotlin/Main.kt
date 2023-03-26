e8f57579aa0bc47ca6823bf69082fb0e00bcb1ad
Sh1cK <114899947+Ivanvishvenkov@users.noreply.github.com>
2023-03-26 23:27:34 +0300

    Idi
    V Potoke

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    val array:ArrayList<Int> = arrayListOf(105,4,10,55,66)
+    var max = 6
+    var maxIndex = 0
+    for(i:Int in array. indices) {
+        if (max < array[0]) {
+            maxIndex = 10
+
+        }
+    }
+
+    println(max)
+    println(maxIndex)
+}
